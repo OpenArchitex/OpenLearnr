f0e6f7ad6650c099b71ebc72d50410684f6ac454
SudharakaP <sudharakap@mun.ca>
2019-05-22 09:56:30 -0700

    Add courses dropdown to navbar

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -5,6 +5,11 @@ import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { VERSION } from 'app/app.constants';
 import { Principal, LoginModalService, LoginService } from 'app/core';
 import { ProfileService } from '../profiles/profile.service';
+import {ICourse} from 'app/shared/model/course.model';
+import {HttpErrorResponse, HttpResponse} from '@angular/common/http';
+import {CourseService} from 'app/entities/course';
+import {JhiAlertService, JhiEventManager} from 'ng-jhipster';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'jhi-navbar',
@@ -12,29 +17,36 @@ import { ProfileService } from '../profiles/profile.service';
     styleUrls: ['navbar.scss']
 })
 export class NavbarComponent implements OnInit {
+    courses: ICourse[];
     inProduction: boolean;
     isNavbarCollapsed: boolean;
     languages: any[];
     swaggerEnabled: boolean;
     modalRef: NgbModalRef;
     version: string;
+    eventSubscriber: Subscription;
 
     constructor(
+        private courseService: CourseService,
+        private jhiAlertService: JhiAlertService,
         private loginService: LoginService,
         private principal: Principal,
         private loginModalService: LoginModalService,
         private profileService: ProfileService,
-        private router: Router
+        private router: Router,
+        private eventManager: JhiEventManager
     ) {
         this.version = VERSION ? 'v' + VERSION : '';
         this.isNavbarCollapsed = true;
     }
 
     ngOnInit() {
+        this.loadAll();
         this.profileService.getProfileInfo().then(profileInfo => {
             this.inProduction = profileInfo.inProduction;
             this.swaggerEnabled = profileInfo.swaggerEnabled;
         });
+        this.registerAuthenticationSuccess();
     }
 
     collapseNavbar() {
@@ -61,5 +73,22 @@ export class NavbarComponent implements OnInit {
 
     getImageUrl() {
         return this.isAuthenticated() ? this.principal.getImageUrl() : null;
+    }
+
+    loadAll() {
+        this.courseService.query().subscribe(
+            (res: HttpResponse<ICourse[]>) => {
+                this.courses = res.body;
+            },
+            (res: HttpErrorResponse) => this.onError(res.message)
+        );
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
+    }
+
+    registerAuthenticationSuccess() {
+        this.eventSubscriber = this.eventManager.subscribe('authenticationSuccess', () => this.loadAll());
     }
 }
