458166ba9984b1fa0f36aab16546184f4eba0959
SudharakaP <sudharakap@mun.ca>
2019-06-02 16:08:10 -0700

    Basic snackbar to welcome user when logging in

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { LoginModalService, Principal, Account } from 'app/core';
 
@@ -13,7 +14,12 @@ export class HomeComponent implements OnInit {
     account: Account;
     modalRef: NgbModalRef;
 
-    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+    constructor(
+        private principal: Principal,
+        private loginModalService: LoginModalService,
+        private eventManager: JhiEventManager,
+        private _snackBar: MatSnackBar
+    ) {}
 
     ngOnInit() {
         this.principal.identity().then(account => {
@@ -26,8 +32,16 @@ export class HomeComponent implements OnInit {
         this.eventManager.subscribe('authenticationSuccess', message => {
             this.principal.identity().then(account => {
                 this.account = account;
+                this._snackBar.open('Welcome ' + this.upperCaseFirstLetter(this.account.login) + '!', null, {
+                    duration: 5000,
+                    panelClass: ['login-snack-bar']
+                });
             });
         });
+    }
+
+    upperCaseFirstLetter(login: string): string {
+        return login.charAt(0).toUpperCase() + login.slice(1);
     }
 
     isAuthenticated() {
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@angular/router';
 
 import { OnlineTutorSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
+import { MatSnackBarModule } from '@angular/material';
 
 @NgModule({
-    imports: [OnlineTutorSharedModule, RouterModule.forChild([HOME_ROUTE])],
+    imports: [OnlineTutorSharedModule, RouterModule.forChild([HOME_ROUTE]), MatSnackBarModule],
     declarations: [HomeComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
