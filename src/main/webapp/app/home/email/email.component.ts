50cd5364d754ba420c7a1859f7efe876e54fbaf3
SudharakaP <sudharakap@mun.ca>
2019-06-06 09:28:12 -0700

    Separate contact form to separate component

---
+++ src/main/webapp/app/home/email/email.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-email',
+    templateUrl: './email.component.html',
+    styleUrls: ['./email.scss']
+})
+export class EmailComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,10 +4,11 @@ import { RouterModule } from '@angular/router';
 import { OnlineTutorSharedModule } from 'app/shared';
 import { HOME_ROUTE, HomeComponent } from './';
 import { MatButtonModule, MatInputModule, MatSnackBarModule } from '@angular/material';
+import { EmailComponent } from './email/email.component';
 
 @NgModule({
     imports: [OnlineTutorSharedModule, RouterModule.forChild([HOME_ROUTE]), MatSnackBarModule, MatButtonModule, MatInputModule],
-    declarations: [HomeComponent],
+    declarations: [HomeComponent, EmailComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class OnlineTutorHomeModule {}
