47671d12ad344df6bf02ba3ff7cd4793a3237baf
SudharakaP <sudharakap@mun.ca>
2019-06-22 19:27:30 -0700

    Add audit logging for payments

--- src/main/java/com/asanka/tutor/web/rest/PaymentController.java
+++ src/main/java/com/asanka/tutor/web/rest/PaymentController.java
@@ -1,6 +1,7 @@
 package com.asanka.tutor.web.rest;
 
 import com.asanka.tutor.domain.User;
+import com.asanka.tutor.repository.CustomAuditEventRepository;
 import com.asanka.tutor.security.StripeClient;
 import com.asanka.tutor.security.UserNotLoggedInException;
 import com.asanka.tutor.service.UserService;
@@ -11,6 +12,7 @@ import io.github.jhipster.web.util.ResponseUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
@@ -30,13 +30,16 @@ public class PaymentController {
     private StripeClient stripeClient;
 
     private final UserService userService;
+    
+    private CustomAuditEventRepository customAuditEventRepository;
 
     private final Logger log = LoggerFactory.getLogger(PaymentController.class);
 
     @Autowired
-    PaymentController(StripeClient stripeClient, UserService userService) {
+    PaymentController(StripeClient stripeClient, UserService userService, CustomAuditEventRepository customAuditEventRepository) {
         this.stripeClient = stripeClient;
         this.userService = userService;
+        this.customAuditEventRepository = customAuditEventRepository;
     }
 
     @PostMapping("/payment")
@@ -51,10 +54,19 @@ public class PaymentController {
             throw new UserNotLoggedInException("User not logged in!");
         }
         Charge charge;
+        AuditEvent event = null;
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", chapterIDs.toString());
         try {
+            // TODO: Change user friendliness
             charge = this.stripeClient.chargeCreditCard(token, stripeClient.getStripeUnitPrice() * length);
+            event = new AuditEvent(user.get().getLogin(), "PAYMENT_SUCCESSFUL", data);
         } catch (CardException e) {
+            // TODO: Change user friendliness
+            event = new AuditEvent(user.get().getLogin(), "PAYMENT_FAILURE", data);
             throw new StripeCardError(e.getMessage());
+        } finally {
+            customAuditEventRepository.add(event);
         }
         for (int i = 0; i < length; i++){
             chapters.add(chapterIDs.get(i).toString());
