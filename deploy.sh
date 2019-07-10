a8a21ed2acd63a97760d03233e2abbd7ed765daf
SudharakaP <sudharakap@mun.ca>
2019-07-10 07:24:10 -0700

    Correct deployment version in the pom file and add proper semvar validation (#3)

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
-if [[ $TRAVIS_TAG =~ ^v(.+) ]]; then
+# Semvar versioning regex (https://github.com/semver/semver/issues/232#issuecomment-405596809)
+SEMVAR="^(?<major>0|[1-9]\d*)\.(?<minor>0|[1-9]\d*)\.(?<patch>0|[1-9]\d*)(?:-(?<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+
+if [[ $TRAVIS_TAG =~ $SEMVAR ]]; then
     # Decrypt the secrets file
     echo $super_secret_password | gpg --passphrase-fd 0 secrets.tar.gpg
     tar xvf secrets.tar
@@ -6,4 +9,3 @@ if [[ $TRAVIS_TAG =~ ^v(.+) ]]; then
     # Deploy on AppEngine
     ./mvnw package appengine:deploy -Pprod,war -DskipTests
 fi
-
