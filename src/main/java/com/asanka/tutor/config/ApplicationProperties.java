131ef718a9638288bcfe39d9b1ca8a0080f6a526
SudharakaP <sudharakap@mun.ca>
2019-07-30 21:09:19 -0700

    Correct minor project naming mistakes (#5)

--- src/main/java/com/asanka/tutor/config/ApplicationProperties.java
+++ src/main/java/com/asanka/tutor/config/ApplicationProperties.java
@@ -3,7 +3,7 @@ package com.asanka.tutor.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Properties specific to Open Learnr.
+ * Properties specific to OpenLearnr.
  * <p>
  * Properties are configured in the {@code application.yml} file.
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
