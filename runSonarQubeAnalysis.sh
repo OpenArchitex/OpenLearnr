d3163808ad24a47604b89c80ccedebf2e7ab1257
SudharakaP <sudharakap@mun.ca>
2019-08-21 23:12:14 -0700

    Simplify Sonar logic (#75)

--- runSonarQubeAnalysis.sh
+++ runSonarQubeAnalysis.sh
@@ -8,16 +8,7 @@ set -e
 
 # And run the analysis
 # It assumes that the project uses Maven and has a POM at the root of the repo
-if [ "$TRAVIS_BRANCH" = "develop" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	# => This will run a full analysis of the project and push results to the SonarQube server.
-	#
-	# Analysis is done only on develop so that build of branches don't push analyses to the same project and therefore "pollute" the results
-	echo "Starting analysis by SonarQube..."
-	mvnw -Pprod clean verify sonar:sonar -e -V \
-		-Dsonar.host.url=$SONAR_HOST_URL \
-		-Dsonar.login=$SONAR_TOKEN
-
-elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
+if [ "$TRAVIS_PULL_REQUEST" == "true" ] && [ -n "${GITHUB_TOKEN-}" ]; then
 	# => This will analyse the PR and display found issues as comments in the PR, but it won't push results to the SonarQube server
 	#
 	# For security reasons environment variables are not available on the pull requests
@@ -32,5 +23,13 @@ elif [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN-}" ]; then
 		-Dsonar.github.oauth=$GITHUB_TOKEN \
 		-Dsonar.github.repository=$TRAVIS_REPO_SLUG \
 		-Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
+else
+    # => This will run a full analysis of the project and push results to the SonarQube server.
+    #
+    # Analysis is done only on develop so that build of branches don't push analyses to the same project and therefore "pollute" the results
+    echo "Starting analysis by SonarQube..."
+    mvnw -Pprod clean verify sonar:sonar -e -V \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
 fi
 # When neither on develop branch nor on a non-external pull request => nothing to do
