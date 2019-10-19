6d43927784f0c237de0f45c0681327339198c360
Pulathisi <pulathisi.basnayake@gmail.com>
2019-10-19 10:01:59 +0530

    email component and responsive ui changes

--- src/main/webapp/app/home/email/email.component.ts
+++ src/main/webapp/app/home/email/email.component.ts
@@ -7,68 +7,68 @@ import { MatSnackBar } from '@angular/material';
 import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
-    selector: 'jhi-email',
-    templateUrl: './email.component.html',
-    styleUrls: ['./email.scss']
+  selector: 'jhi-email',
+  templateUrl: './email.component.html',
+  styleUrls: ['./email.component.scss']
 })
 export class EmailComponent implements OnInit {
-    contactForm = this.fb.group({
-        contactName: ['', Validators.required],
-        contactEmail: ['', [Validators.required, Validators.email]],
-        contactPhone: [''],
-        contactMsg: ['', Validators.required]
-    });
+  contactForm = this.fb.group({
+    contactName: ['', Validators.required],
+    contactEmail: ['', [Validators.required, Validators.email]],
+    contactPhone: [''],
+    contactMsg: ['', Validators.required]
+  });
 
-    get contactName() {
-        return this.contactForm.get('contactName');
-    }
+  get contactName() {
+    return this.contactForm.get('contactName');
+  }
 
-    get contactEmail() {
-        return this.contactForm.get('contactEmail');
-    }
+  get contactEmail() {
+    return this.contactForm.get('contactEmail');
+  }
 
-    get contactPhone() {
-        return this.contactForm.get('contactPhone');
-    }
+  get contactPhone() {
+    return this.contactForm.get('contactPhone');
+  }
 
-    get contactMsg() {
-        return this.contactForm.get('contactMsg');
-    }
+  get contactMsg() {
+    return this.contactForm.get('contactMsg');
+  }
 
-    onSubmit() {
-        this.sendRequest().subscribe(
-            () => {
-                this._snackBar.open('Thanks for writing to us! We will get back to you soon!', null, {
-                    duration: 5000,
-                    panelClass: ['contact-form-snack-bar']
-                });
-                this.contactForm.reset();
-            },
-            err => {
-                this.onError(err.message);
-            }
-        );
-    }
-
-    sendRequest(): Observable<any> {
-        return this.http.post<any>('https://usebasin.com/f/b5ed73d03aa5', this.contactForm.value, {
-            headers: new HttpHeaders({
-                Accept: 'application/json'
-            }),
-            observe: 'response'
+  onSubmit() {
+    this.sendRequest().subscribe(
+      () => {
+        this._snackBar.open('Thanks for writing to us! We will get back to you soon!', null, {
+          duration: 5000,
+          panelClass: ['contact-form-snack-bar']
         });
-    }
+        this.contactForm.reset();
+      },
+      err => {
+        this.onError(err.message);
+      }
+    );
+  }
 
-    constructor(
-        private fb: FormBuilder,
-        private http: HttpClient,
-        private _snackBar: MatSnackBar,
-        private jhiAlertService: JhiAlertService
-    ) {}
+  sendRequest(): Observable<any> {
+    return this.http.post<any>('https://usebasin.com/f/b5ed73d03aa5', this.contactForm.value, {
+      headers: new HttpHeaders({
+        Accept: 'application/json'
+      }),
+      observe: 'response'
+    });
+  }
 
-    ngOnInit() {}
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private _snackBar: MatSnackBar,
+    private jhiAlertService: JhiAlertService
+  ) {}
 
-    private onError(errorMessage: string) {
-        this.jhiAlertService.error(errorMessage, null, null);
-    }
+  ngOnInit() {}
+
+  private onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
+  }
 }
